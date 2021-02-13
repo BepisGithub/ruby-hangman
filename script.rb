dc5ac28e3d07649377cb7a798cd5eb4b6d0e5e5a
bepis <luqdek@gmail.com>
2021-02-13 16:11:17 +0000

    refactored code

--- script.rb
+++ script.rb
@@ -1,20 +1,28 @@
-dictionary = File.open('5desk.txt', 'r')
+$dictionary = File.open('5desk.txt', 'r')
 
 class Game
   
-  def round
 
-  end
   
   def initialize(save_game = false)
     # check for save game later, if true load it and set the parameters. else use default values.
-    secret_word = File.readlines(dictionary).sample
-    false_guesses = 0
-    guessed = false
-    while false_guesses < 6 || !(guessed)
-      round
+    @secret_word = File.readlines($dictionary).sample
+    @false_guesses = 0
+    @guessed = false
+    game(@secret_word, @false_guesses, @guessed)
+  end
+
+  def round(word, f_guesses, guessed)
+    puts word
+  end
+
+  def game(word, f_guesses, guessed)
+    while f_guesses < 6 && !guessed
+      guessed = round(word, f_guesses, guessed)
     end
   end
   
 
 end
+
+a = Game.new
